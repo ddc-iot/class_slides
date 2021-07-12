5080f70d75164a01f17f173ac7d988cc3bf3a3f4
brashap <brashap@gmail.com>
2021-07-12 11:40:57 -0600

    updated slides

--- ParticleCode/L14_03_SubscribePublish/src/credentials.h
+++
@@ -1,5 +0,0 @@
-/************************* Adafruit.io Setup *********************************/ 
-#define AIO_SERVER      "io.adafruit.com" 
-#define AIO_SERVERPORT  1883                   // use 8883 for SSL 
-#define AIO_USERNAME  "rashap"
-#define AIO_KEY       "598169fa3ba94636941a330976e42e2c"
